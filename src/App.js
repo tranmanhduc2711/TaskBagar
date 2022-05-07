e2325fb27377e8cbdf669cf584a85d6f05f39e1c
tranmanhduc2711 <manhduc2711@gmail.com>
2022-05-07 17:01:34 +0700

    hompage UI

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './style/_reset.scss';
 import "./style/_global.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import AddNewProject from './components/AddProject/AddProject';
+import Homepage from './components/Homepage/Homepage';
 function App() {
   return (
     <>
       <Router>
         <div className="App">
-          <Header/>
+          <Header />
           <Routes>
-            {/* <Route exact path="/login" element={<Login />} /> */}
+            <Route exact path="/addNewProject" element={<AddNewProject />} />
+            <Route exact path="/" element={<Homepage />} />
           </Routes>
         </div>
       </Router>
